e11283c783ddf6df4426352bc635c97eec6c5e59
BensonJing <bensonjing@icloud.com>
2022-03-17 17:30:43 -0700

    add computerPlay() and playRound()

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 function computerPlay(){
-    const choices = ['Rock', 'Paper', 'Scissor']; 
+    const choices = ['rock', 'paper', 'scissors']; 
     const random = Math.floor(Math.random() * choices.length); 
     return choices[random]; 
 } 
+
+function playRound(playerSelection, computerSelection){
+    const p = playerSelection.toLowerCase();
+    const c = computerSelection; 
+    if (playerSelection == computerSelection) {
+        return 'Tie'; 
+    } else if (p == 'rock'){
+        if (c == 'paper'){
+            return `You Lose! ${c} beats ${p}`; 
+        } else if (c == 'scissors') {
+            return `You Win! ${p} beats ${c}`; 
+        }
+    } else if (p == 'paper') {
+        if (c == 'rock') {
+            return `You Win! ${p} beats ${c}`; 
+        } else if (c == 'scissors') {
+            return `You Lose! ${c} beats ${p}`; 
+        }
+    } else if (p == 'scissors') {
+        if (c == 'rock') {
+            return `You Lose! ${c} beats ${p}`; 
+        } else if (c == 'paper') {
+            return `You Win! ${p} beats ${c}`; 
+        }
+    }
+}
 
