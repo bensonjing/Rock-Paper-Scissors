7c101edd109646fa3bcad90deb213758568345d4
BensonJing <bensonjing@icloud.com>
2022-03-24 21:24:08 -0700

    add GUI

--- main.js
+++ main.js
@@ -1,48 +1,58 @@
+let pwin = 0, cwin = 0, tie = 0; 
+
+const buttons = document.querySelectorAll('button'); 
+const div = document.querySelector('div'); 
+
 function computerPlay(){
     const choices = ['rock', 'paper', 'scissors']; 
     const random = Math.floor(Math.random() * choices.length); 
     return choices[random]; 
-} 
+};
 
 function playRound(playerSelection, computerSelection){
     const p = playerSelection.toLowerCase(), c = computerSelection; 
     if (playerSelection == computerSelection) {
-        return ['Tie', 'nobody'];  
+        tie += 1; 
+        div.textContent = `${p} and ${c} is a tie.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;   
     } else if (p == 'rock'){
         if (c == 'paper'){
-            return [`You Lose! ${c} beats ${p}`, 'c']; 
+            cwin += 1;  
+            div.textContent = `You Lost! ${c} beats ${p}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;   
         } else if (c == 'scissors') {
-            return [`You Win! ${p} beats ${c}`, 'p']; 
+            pwin += 1; 
+            div.textContent = `You won! ${p} beats ${c}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;    
         }
     } else if (p == 'paper') {
         if (c == 'rock') {
-            return [`You Win! ${p} beats ${c}`, 'p']; 
+            pwin += 1; 
+            div.textContent = `You won! ${p} beats ${c}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;    
         } else if (c == 'scissors') {
-            return [`You Lose! ${c} beats ${p}`, 'c']; 
+            cwin += 1; 
+            div.textContent = `You Lost! ${c} beats ${p}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;   
         }
     } else if (p == 'scissors') {
         if (c == 'rock') {
-            return [`You Lose! ${c} beats ${p}`, 'c']; 
+            cwin += 1; 
+            div.textContent = `You Lost! ${c} beats ${p}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;   
         } else if (c == 'paper') {
-            return [`You Win! ${p} beats ${c}`, 'p'];  
+            pwin += 1; 
+            div.textContent = `You won! ${p} beats ${c}.` + `You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`;     
         }
     }
 }
 
 function game(){
-    let pwin = 0, cwin = 0, tie = 0; 
-    for (let i = 0; i < 5; i++){
-        const [result, winner] = playRound(prompt('Enter your move[rock, paper, scissors]: '), computerPlay()); 
-        if (winner == 'p'){
-            pwin += 1; 
-        } else if (winner == 'c') {
-            cwin += 1; 
-        } else {
-            tie += 1; 
-        }
-        console.log(result); 
+    if (pwin == 5) {
+        alert('Player is the winner!'); 
+    } else if (cwin == 5) {
+        alert('Computer is the winner!'); 
     }
-    console.log(`You won ${pwin} times. Computer won ${cwin} times. You tied ${tie} times`); 
 }
 
-game()
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+        game();  
+    }); 
+}); 
